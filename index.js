7570fa3d946b6a541382f0d5cb4fe3dd3295ba3d
rakibul58 <rhrahi14@gmail.com>
2022-11-03 18:36:28 +0600

    server added

---
+++ index.js
@@ -0,0 +1,60 @@
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.bebjeyw.mongodb.net/?retryWrites=true&w=majority`;
+
+
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+
+    try{
+        const productCollection = client.db('emaJohn').collection('products');
+
+        app.get('/products' , async(req , res)=>{
+            const page = parseInt(req.query.page);
+            const size = parseInt(req.query.size);
+            console.log(page, size);
+            const query = {};
+            const cursor = productCollection.find(query);
+            const products = await cursor.skip(page*size).limit(size).toArray();
+            const count = await productCollection.estimatedDocumentCount();
+            res.send({count , products});
+        });
+
+        app.post('/productsByIds' , async(req , res)=>{
+            
+            const ids = req.body;
+            const objectIds = ids.map(id => ObjectId(id));
+            const query = {_id: {$in: objectIds}};
+            const cursor = productCollection.find(query);
+            const products = await cursor.toArray();
+            res.send(products);
+
+        });
+    }
+    finally{
+
+    }
+
+}
+
+run().catch(err=>console.error(err));
+
+app.get('/',(req , res)=>{
+    res.send('Ema John server is running');
+});
+
+app.listen(port , ()=>{
+    console.log(`ema john running on port ${port}`);
+})
